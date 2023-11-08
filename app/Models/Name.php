6808f01a840481a2e240070aa0cba456b7cb3889
misi1239 <dankomisi1@gmail.com>
2023-11-08 22:31:48 +0100

    create models and migrations file for the database structure

---
+++ app/Models/Name.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Name extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nev',
+        'address',
+        'email_address',
+        'photo',
+    ];
+
+    public function emails()
+    {
+        return $this->hasMany(Email::class);
+    }
+
+    public function phones()
+    {
+        return $this->hasMany(Phone::class);
+    }
+}
---
+++ database/migrations/2023_11_08_211451_create_names_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('names', function (Blueprint $table) {
+            $table->id();
+            $table->string('nev');
+            $table->string('address')->nullable();
+            $table->string('mailing address')->nullable();
+            $table->binary('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('names');
+    }
+};
