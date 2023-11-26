894f4c0e2256f5fb4e30cea91fd7f8463cbdb1dd
misi1239 <dankomisi1@gmail.com>
2023-11-26 19:51:13 +0100

    fixed image tag in index.vue

--- resources/js/components/phone-book/index.vue
+++ resources/js/components/phone-book/index.vue
@@ -66,13 +66,10 @@
                             <tr>
                                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                     <div class="flex">
-                                        <div class="flex-shrink-0 w-10 h-10">
-                                            <img class="w-full h-full rounded-full"
-                                                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                                alt=""
-                                            />
+                                        <div class="flex-shrink-0 w-10 h-10" v-if="item.image_path">
+                                            <img class="shadow max-w-full h-auto border-none" :src="'/images/' + item.image_path" alt="">
                                         </div>
-                                        <div class="ml-3">
+                                        <div class="ml-3 px-5 py-5">
                                             <p class="text-gray-900 whitespace-no-wrap">{{ item.name }}</p>
                                         </div>
                                     </div>
